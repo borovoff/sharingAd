ef85c0a015550771013bbcc573eaffa2be22285f
borovoff <borovov.alexandr@gmail.com>
2021-01-18 21:18:36 +0300

    remove html extensions

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const footer = document.createElement('footer')
 footer.classList.add('footer', 'row', 'row_evenly', 'row_wrap')
 document.body.appendChild(footer)
 
-fetch('footer.html').then(async result => footer.innerHTML = await result.text())
+fetch('footer').then(async result => footer.innerHTML = await result.text())
